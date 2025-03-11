51e8a69cbf6633e289c479e7a7ae7baaa9e35ce4
zzq <857565478@qq.com>
2025-03-11 02:39:51 +0000

    nvm地址映射器和访问器的基本实现

---
+++ src/accessor.h
@@ -0,0 +1,24 @@
+#ifndef ACCESSOR
+#define ACCESSOR
+
+#include "nvm_addr_map_manager.h"
+
+typedef struct Accessor{
+    NvmAddrMapManager* nvm_addr_map_manager;
+    // TODO 内存保护：需要访问的时候建立地址映射
+}Accessor;
+
+/***********************public API***********************/
+
+Accessor* accessor_init(NvmAddrMapManager* nvm_addr_map_manager);
+
+int accessor_destory(Accessor* this);
+
+void* get_vaddr_by_paddr(Accessor* this, phys_addr_t paddr);
+
+// 一个lab对应一个扇区，一个扇区假定是512字节
+void* get_vaddr_by_lba(Accessor* this, u64 lba);
+
+/***********************public API***********************/
+
+#endif
---
+++ src/nvm_addr_map_manager.c
@@ -0,0 +1,104 @@
+#include "nvm_addr_map_manager.h"
+
+#include <linux/slab.h>
+#include <linux/mm.h>
+#include <linux/io.h>
+
+
+/***********************private API***********************/
+
+void* phy_addr_map_virt_addr(NvmAddrMapManager *this, phys_addr_t nvm_phy_addr){
+    long offset_of_paddr_and_vaddr = (unsigned long)this->nvm_virt_start_addr - this->nvm_phy_start_addr;
+    return (void *) (nvm_phy_addr += offset_of_paddr_and_vaddr);
+}
+
+/***********************private API***********************/
+
+/***********************public API***********************/
+
+NvmAddrMapManager* nvm_addr_map_manager_init(
+        phys_addr_t nvm_phy_start_addr, unsigned long nvm_phy_length){
+    
+    NvmAddrMapManager *nvm_addr_map_manager;
+    phys_addr_t aligned_nvm_phy_start_addr, aligned_nvm_phy_end_addr;
+    void *nvm_virt_start_addr;
+    unsigned long aligned_nvm_phy_length, nvm_virt_length;;
+
+    aligned_nvm_phy_start_addr = nvm_phy_start_addr;
+    aligned_nvm_phy_length     = nvm_phy_length;
+    // 4K对齐 // ! 不能对物理地址进行裁剪
+    // aligned_nvm_phy_start_addr = (nvm_phy_start_addr + PAGE_ALIGN_4K - 1) & PAGE_ALIGN_MASK_4K;
+    // aligned_nvm_phy_end_addr   = (nvm_phy_start_addr + nvm_phy_length) & PAGE_ALIGN_MASK_4K;
+
+    // if(aligned_nvm_phy_start_addr >= aligned_nvm_phy_end_addr){
+    //     pr_err("aligned physical range wrong!\n");
+    //     goto aligned_fail;
+    // }
+    // aligned_nvm_phy_length = aligned_nvm_phy_end_addr - aligned_nvm_phy_start_addr;
+
+    // 建立映射 //? 映射函数是否需要抽象出来？
+    if (!request_mem_region(aligned_nvm_phy_start_addr, aligned_nvm_phy_length, "memremap")) {
+        pr_err("request_mem_region fail!\n");
+        goto request_mem_region_fail;
+    }
+    nvm_virt_start_addr = memremap(aligned_nvm_phy_start_addr, aligned_nvm_phy_length, MEMREMAP_WB);
+    if (!nvm_virt_start_addr) {
+        pr_err("memremap fail!\n");
+        goto memremap_fail;
+    }
+
+    // 申请管理器
+    nvm_addr_map_manager = kmalloc(sizeof(NvmAddrMapManager), GFP_KERNEL);
+    if(nvm_addr_map_manager == NULL){
+        pr_err("kmalloc NvmAddrMapManager fail!\n");
+        goto kamlloc_fail;
+    }
+    nvm_virt_length = aligned_nvm_phy_length;
+    nvm_addr_map_manager->nvm_phy_start_addr          = aligned_nvm_phy_start_addr;
+    nvm_addr_map_manager->nvm_phy_length              = aligned_nvm_phy_length;
+    nvm_addr_map_manager->nvm_virt_start_addr         = nvm_virt_start_addr;
+    nvm_addr_map_manager->nvm_virt_length             = aligned_nvm_phy_length;
+    nvm_addr_map_manager->phy_addr_map_virt_addr_func = phy_addr_map_virt_addr;
+
+    return nvm_addr_map_manager;
+
+kamlloc_fail:
+    memunmap(&aligned_nvm_phy_start_addr);   // ? 为什么不用提供内存长度
+memremap_fail:
+    release_region(aligned_nvm_phy_start_addr, aligned_nvm_phy_length);
+request_mem_region_fail:
+aligned_fail:
+    return NULL;
+}
+
+int nvm_addr_map_manager_destory(NvmAddrMapManager* this){
+    if(!this){
+        pr_err("NvmAddrMapManager pointer unvaild!\n");
+        return EINVAL;
+    }
+
+    memunmap(&this->nvm_phy_start_addr);
+    release_region(this->nvm_phy_start_addr, this->nvm_phy_length);
+    kfree(this);
+    return 0;
+}
+
+void* nvm_phy_addr_map_virt_addr(NvmAddrMapManager *this, phys_addr_t phy_addr){
+    void* nvm_virt_addr;
+
+    if(this->nvm_phy_start_addr > phy_addr ||
+       this->nvm_phy_start_addr + this->nvm_phy_length < phy_addr){
+        pr_info("nvm_phy_addr out of the mapped phyaddr!\n");
+        return NULL;
+    }
+    
+    nvm_virt_addr = this->phy_addr_map_virt_addr_func(this, phy_addr);
+    if(!nvm_virt_addr){
+        pr_info("phy_addr_map_virt_addr fail!\n");
+        return NULL;
+    }
+
+    return nvm_virt_addr;
+}
+
+/***********************public API***********************/
